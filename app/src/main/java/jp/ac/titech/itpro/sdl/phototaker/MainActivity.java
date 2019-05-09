4cd5714fc149dca682193187b7e73441fa5ea6b7
shoman <shoma216@gmail.com>
2019-05-10 00:33:18 +0900

    [Extra hw] fix image more clear using an external strage

--- app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/phototaker/MainActivity.java
@@ -3,7 +3,10 @@ package jp.ac.titech.itpro.sdl.phototaker;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +14,31 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
     private Bitmap photoImage = null;
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+        return image;
+    }
+
+    private Uri photoURI = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +55,20 @@ public class MainActivity extends AppCompatActivity {
                 PackageManager manager = getPackageManager();
                 List activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
                 if (!activities.isEmpty()) {
-                    startActivityForResult(intent, REQ_PHOTO);
+                    File photoFile = null;
+                    try {
+                        photoFile = createImageFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    if (photoFile != null) {
+                        // URIを取得
+                        photoURI = FileProvider.getUriForFile(MainActivity.this,
+                                getApplicationContext().getPackageName() + ".fileprovider", photoFile);
+                        // putしてもonActivityResultではnullになる
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                        startActivityForResult(intent, REQ_PHOTO);
+                    }
                 } else {
                     Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
                 }
@@ -56,8 +91,15 @@ public class MainActivity extends AppCompatActivity {
             case REQ_PHOTO:
                 if (resCode == RESULT_OK) {
                     // TODO: You should implement the code that retrieve a bitmap image
-                    Bundle extras = data.getExtras();
-                    photoImage = (Bitmap) extras.get("data");
+//                    try {
+//                        photoImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+
+                    // URIからphotoをセット
+                    ImageView photoView = findViewById(R.id.photo_view);
+                    photoView.setImageURI(photoURI);
                 }
                 break;
         }
